e6a5f3d9bd49d298c7f308d3e6b2d74c801c51e9
kennedypascal <pascal.kwame@outlook.com>
2022-12-08 05:17:52 -0800

    4 puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Description: Programming is like building a multilingual puzzle
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
