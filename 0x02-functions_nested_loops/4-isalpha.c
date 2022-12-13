f2d906b476e90ddee3afa128872697a2fb5d0020
kennedypascal <pascal.kwame@outlook.com>
2022-12-13 11:54:46 -0800

    functions and nested loop

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * isalpha - Shows 1 if the input is a
+ * _isalpha - Shows 1 if the input is a
  * letter Another cases, shows 0
  *
  * @c: The character in ASCII code
  *
  * Return: 1 for letters. 0 for the rest.
  */
-int isalpha(int c)
+int _isalpha(int c)
 {
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
