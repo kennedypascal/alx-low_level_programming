765d715f09bd0fbddc657f323386286da3550116
kennedypascal <pascal.kwame@outlook.com>
2022-12-13 05:38:42 -0800

    functions and nested loop

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,14 +2,14 @@
 #include "main.h"
 
 /**
- * main - function
+ * print_alphabet - print alphabet
  *
  * description: function to print out alphabets in lowercase
  *
  * Return: Always 0 (Success)
  */
 
-void print_alphabet (void)
+void print_alphabet(void)
 {
 	int i;
 
