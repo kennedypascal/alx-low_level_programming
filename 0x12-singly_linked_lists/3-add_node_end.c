465ebbb6410ef6589c3b93b1d8bf387c9c602e2e
kennedypascal <pascal.kwame@outlook.com>
2023-01-26 11:58:14 -0800

    add node end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,28 +11,40 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new, *temp;
-	size_t nchar;
+	list_t *new_node, *temp;
+	size_t str_len = 0;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
+	/*set strig length to 0 if its NULL*/
+	if (str == NULL)
+		str_len = 0;
+	/*count length of string*/
+	while (str[str_len] != '\0')
+		str_len++;
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
 		return (NULL);
-	new->str = strdup(str);
-	for (nchar = 0; str[nchar]; nchar++)
-		;
-	new->len = nchar;
-	new->next = NULL;
+
+	new_node->str = strdup(str);
+	new_node->len = str_len;
+	new_node->next = NULL;
+
+	/*copy our starting node pointer to temp*/
 	temp = *head;
 
+	/*if temp is NULL place our new_node at the beginning of the list*/
+	/*else if its not null we place our new_node to temp->next hence*/
+	/*adding our new_node at the end of the list*/
 	if (temp == NULL)
-	{
-		*head = new;
-	}
+		*head = new_node;
 	else
 	{
+		/**
+		 * we iterate pointer next addr if its not NULL our temp addr
+		 * points to the next addr or the next node.
+		*/
 		while (temp->next != NULL)
 			temp = temp->next;
-		temp->next = new;
+		temp->next = new_node;
 	}
 	return (*head);
 }
