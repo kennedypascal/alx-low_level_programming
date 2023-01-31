627bacbf17d81ff63c0ae7fd01548ed071eda7eb
kennedypascal <pascal.kwame@outlook.com>
2023-01-31 14:05:42 -0800

    linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,7 +10,6 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t node_count = 1;
-
 	/* return 0 as no of nodes when h is null*/
 	if (h == NULL)
 		return (0);
@@ -18,13 +17,11 @@ size_t print_listint(const listint_t *h)
 	while (h->next != NULL)
 	{
 		printf("%d\n", h->n);
-
 		/*go to the next node*/
 		h = h->next;
 		/*count no of nodes*/
 		node_count++;
 	}
-
 	/*print last node*/
 	printf("%d\n", h->n);
 
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,12 +26,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			h = h->next;
 		}
 	}
+
 	if (h == NULL && idx != 0)
 		return (NULL);
 
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
+
 	new->n = n;
 
 	if (idx == 0)
@@ -44,5 +46,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new->next = h->next;
 		h->next = new;
 	}
+
 	return (new);
 }
